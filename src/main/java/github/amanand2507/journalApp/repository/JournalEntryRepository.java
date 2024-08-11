d67a3897903567fb57e1765044fc3596e1dc09b3
amanand2507 <amanand257@gmail.com>
2024-08-11 10:00:00 +0530

    add repositories

---
+++ src/main/java/github/amanand2507/journalApp/repository/JournalEntryRepository.java
@@ -0,0 +1,9 @@
+package github.amanand2507.journalApp.repository;
+
+import github.amanand2507.journalApp.entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
+
+}
---
+++ src/main/java/github/amanand2507/journalApp/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package github.amanand2507.journalApp.repository;
+
+import github.amanand2507.journalApp.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+    User findByUserName(String username);
+
+    void deleteByUserName(String username);
+}
